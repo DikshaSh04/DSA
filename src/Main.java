711e7e6020b075b14fd11b35035626c85cf742c2
Diksha <diksha250406@gmail.com>
2025-09-07 17:39:15 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //Algorithm to display first 20 fibonacci numbers using recursion.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static int GlobalCounter=2; //Step1: Define a global counter of no. of fibonacci numbers already displayed(0 and 1).
+    //Step2: Create a function.
+    public static void Fibonacci(int a,int b){
+        if(GlobalCounter<=19){
+            int NewFibo=a+b;
+            System.out.println(NewFibo);
+            a=b;
+            b=NewFibo;
+            GlobalCounter+=1;
+            Fibonacci(a,b); //Step3:Recursion
+        }
+        else{
+            return;
+        }
+
+
+     }
+    public static void main(String[] args) {
+        System.out.println(0);
+        System.out.println(1); //Step4: Display first 2 fibonacci numbers.
+        Fibonacci(0,1);   //Step5: Call function.
+
+
         }
     }
-}
--- src/myFirstAlgo.java
+++ src/myFirstAlgo.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class myFirstAlgo {
+    //This is a program to define an algorithm to print the first 20 fibonacci numbers using loop and recursion.
+    //This allows us to understand how an algorithm helps to solve a problem and it can be implemented in different ways.
+    public static void main(String[] args) {
+
+        //Algorithm to display first 20 fibonacci numbers using 'for' loop.
+
+        int a = 0;
+        int b = 1; //Step1: Initialize the first two fibonacci numbers.
+        System.out.println(a);
+        System.out.println(b); //Step2: Display the first two fibonacci numbers.
+
+        for (int i = 0; i < 18; i++) {//Step3: Create a loop that runs 18 times.
+            int NewFibo = a + b; //Step4: Create a new variable for the next fibonacci number.
+
+            System.out.println(NewFibo); //Step5: Display the new fibonacci number.
+            a = b;
+            b = NewFibo; //Step6: Update the variables that hold the previous two fibonacci numbers.
+
+
+
+
+
+
+
+
+
+        }
+    }
 }
